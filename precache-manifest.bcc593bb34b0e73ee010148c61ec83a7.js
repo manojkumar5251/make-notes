1be824e3731420d8f5d29166b81524e69a78191d
Manoj Kumar <manojkumar5251@gmail.com>
2020-08-04 16:16:11 +0530

    Updates

---
+++ precache-manifest.bcc593bb34b0e73ee010148c61ec83a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352bed19c4460a50afc922509df83f04",
+    "url": "/make-notes/index.html"
+  },
+  {
+    "revision": "c0996b565f984327c32a",
+    "url": "/make-notes/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "c42bb4f30bbba275ca4e",
+    "url": "/make-notes/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "c0996b565f984327c32a",
+    "url": "/make-notes/static/js/2.116a06c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/make-notes/static/js/2.116a06c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42bb4f30bbba275ca4e",
+    "url": "/make-notes/static/js/main.d189e7ce.chunk.js"
+  },
+  {
+    "revision": "fc1482d70751f56f4afc",
+    "url": "/make-notes/static/js/runtime-main.0b7536e0.js"
+  }
+]);
